fb2473ea3118614a1657571e36a7f459eb4b929f
PyJess <x.hita@reply.it>
2025-10-30 17:46:32 +0100

    update tracciabilita

--- Processing/copertura_tracciabilia.py
+++ Processing/copertura_tracciabilia.py
@@ -12,6 +12,7 @@ from openpyxl.styles import Font
 from openpyxl.utils import get_column_letter
 from openpyxl import Workbook
 from openpyxl.styles import PatternFill
+import asyncio
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from utils.simple_functions import group_by_funzionalita, load_json
@@ -21,17 +22,17 @@ from Processing.test_design import create_vectordb
 
 embedding_model = "text-embedding-3-large"
 
-async def prepare_prompt(input: str, context:str = None, testcase: Dict = None) -> Tuple[List[Dict[str, str]], Dict[str, Any]]:
+async def prepare_prompt(input: str, context:str ="", testcase: Dict = None) -> Tuple[List[Dict[str, str]], Dict[str, Any]]:
     """Prepare prompt for the LLM"""
     system_prompt = load_file(os.path.join(os.path.dirname(__file__), "..", "llm", "prompts", "copertura_tracciabilita", "system_prompt.txt"))
     user_prompt = load_file(os.path.join(os.path.dirname(__file__), "..", "llm", "prompts", "copertura_tracciabilita", "user_prompt.txt")) 
-    schema = load_json(os.path.join(os.path.dirname(__file__), "..", "llm", "schema", "schema_output_tracciabilità.json"))
+    schema = load_json(os.path.join(os.path.dirname(__file__), "..", "llm", "schema", "schema_output_tracciabilita.json"))
 
     user_prompt = user_prompt.replace("{input}", input)
     print(f"input {input}")
     user_prompt = user_prompt.replace("{context}", context )
-    testcase= testcase.to_json() 
-    user_prompt = user_prompt.replace("{mapping}", testcase )
+    testcase= json.dumps(testcase, ensure_ascii=False, indent=2)
+    user_prompt = user_prompt.replace("{testcase}", testcase )
     print("finishing prepare prompt")
 
     messages = [
@@ -42,13 +43,12 @@ async def prepare_prompt(input: str, context:str = None, testcase: Dict = None) 
     return messages, schema
 
 
-async def AI_gen_title(input: str, context:str, mapping: str = None) -> Dict:
-    messages, schema = await prepare_prompt(input, context, mapping)
+async def AI_gen_title(input: str, context:str, testcase: Dict = None) -> Dict:
+    messages, schema = await prepare_prompt(input, context, testcase)
     print("starting calling llm")
     #print(f"{messages}")
     response = await a_invoke_model(messages, schema, model="gpt-4.1")
     return response
-
 
 
 
@@ -57,21 +57,32 @@ async def main():
     dic = excel_to_json(excel_path) 
     print("finishing excel to json")
 
-    
-    rag_path= input_path=os.path.join(os.path.dirname(__file__), "..", "input", "Esempio 2", "RU_Sportsbook_Platform_Fantacalcio_Prob. Form_v0.2 (1).docx")
-    chunks, _ = process_docx(input_path, os.path.dirname(rag_path))
+    rag_path = os.path.join(os.path.dirname(__file__), "..", "input", "Esempio 2", 
+                            "RU_Sportsbook_Platform_Fantacalcio_Prob. Form_v0.2 (1).docx")
+    chunks, _ = process_docx(rag_path, os.path.dirname(rag_path))
     embeddings = OpenAIEmbeddings(model=embedding_model)
     vectorstore = FAISS.from_texts(chunks, embeddings)
 
-    for testcase in dic:
-        Title=testcase['Title']
-        new_Title= AI_gen_title(Title, vectorstore, embedding_model)
-        testcase['Title']=new_Title
+    for test_id, testcase in dic.items():
+        Title = testcase['Title']
+        tc=str(testcase)
+        context = create_vectordb(tc, vectorstore, k=3, similarity_threshold=0.75)
+        context=""
+        new_Title = await AI_gen_title(Title, context, testcase)
+        testcase['Title'] = new_Title["corrected_title"]
+        print(f"Corrected Title for {test_id}: {new_Title}")
 
+    
+    excel_path = os.path.join(os.path.dirname(__file__), "..", "outputs", "testbook_tracciabilita_feedbackAI.xlsx")
+    os.makedirs(os.path.dirname(excel_path), exist_ok=True)
 
-    output_excel_path = os.path.join(os.path.dirname(__file__), "..", "outputs", "generated_test_cases3_withDHW.xlsx")
-    convert_json_to_excel(dic, output_excel_path)
+    df1=convert_to_DF(dic)
+    df1.to_excel(excel_path, index=False)
 
+    wb = load_workbook(excel_path)
+    ws = wb.active
 
+    wb.save(excel_path)
 
-
+if __name__ == "__main__":
+    asyncio.run(main())
